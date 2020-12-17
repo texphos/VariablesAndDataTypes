d24ffa3beaee4b4b5470ca3e6438ea46a7ba9d10
Ali Moulton <texphos@gmail.com>
2020-12-17 16:39:59 -0500

    First commit

---
+++ main.py
@@ -0,0 +1,9 @@
+character_name = "Ali"
+character_age = 19
+is_male = True
+print("There once was a man named " + character_name + ",")
+print("he was " + str(character_age) + " years old.")
+
+character_name = "Peetie"
+print("He really liked the name " + character_name + ",")
+print("but didn't like being " + str(character_age) + ".")
